7fbd3cd9dce68a23a6683acd5f2230fb4de36478
Akanewell <jj.almirol025@gmail.com>
2026-03-18 16:15:26 +0800

    Add files via upload
    Seatwork 8.1: Progress Report

---
+++ Novice.py
@@ -0,0 +1,10 @@
+from character import Character
+
+class Novice(Character):
+    def basicAttack(self, character):
+        character.reduceHp(self.getDamage())
+        print(f"{self.getUsername()} performed Basic Attack! -{self.getDamage()}")
+
+character1 = Novice("Joel a almirol")
+print(character1.getUsername())
+print(character1.getHp())
